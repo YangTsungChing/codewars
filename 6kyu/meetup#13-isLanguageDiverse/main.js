f29caecad611e5052996626e47c799b2fee16e76
Jackson Yang <jackson4100052139@gmail.com>
2024-01-13 02:34:31 +0800

    s

--- 6kyu/meetup#13-isLanguageDiverse/main.js
+++ 6kyu/meetup#13-isLanguageDiverse/main.js
@@ -1,3 +1,11 @@
+/*DESCRIPTION:
+You will be given an array of objects representing data about developers who have signed up to attend the next web development meetup that you are organising. Three programming languages will be represented: Python, Ruby and JavaScript.
+
+Your task is to return either:
+
+true if the number of meetup participants representing any of the three programming languages is ** at most 2 times higher than the number of developers representing any of the remaining programming languages**; or
+false otherwise.*/
+
 function isLanguageDiverse(arr) {
     //need to check if the nubmers of  devs of one language is at most 2 times higer than the ohthers
     //to do this , we need to loop through it and get the count of each language and then 
